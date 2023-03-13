8c53b12450d1130886c86e95a6625aeaab97e7ff
Lucas De Smet <lucas.desmet@outlook.com>
2023-03-13 21:49:30 +0700

    Add event loop and key press events

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pygame import *
+import sys
 
 # prepare window
 w_width = 1052
@@ -9,17 +10,40 @@ display.set_caption("First application")
 # set window background
 background = transform.scale(image.load(
     r'pictures\backgrounds\galaxy_2.jpg'), (w_width, w_height))
-window.blit(background, (0, 0))
 
-# add character
-height = 100
-width = 100
+
+# add ufo
+ufo_h = 100
+ufo_w = 100
+# draw.rect(window, (0, 0, 255), (x, y, ufo_w, ufo_h))
+ufo_img = transform.scale(image.load(
+    r'pictures\heroes\ufo_1.png'), (ufo_w, ufo_h))
+
 x = 100
 y = 100
-
-# draw.rect(window, (0, 0, 255), (x, y, width, height))
-img1 = transform.scale(image.load(
-    r'pictures\heroes\ufo_1.png'), (width, height))
-window.blit(img1, (x, y))
-display.update()
-time.delay(5000)
+dist = 10
+run = True
+while run:
+    # erase - TODO: follow https://www.pygame.org/docs/tut/MoveIt.html
+    window.blit(background, (0, 0))
+    # loop runs every 0.1 second
+    time.delay(50)
+    # sort through all the events that could have happened
+    for e in event.get():
+        if e.type == QUIT:
+            sys.exit()
+        if e.type == KEYDOWN:
+            keys = key.get_pressed()
+            # check which buttons were pressed
+            if keys[K_LEFT]:
+                x -= dist
+            if keys[K_RIGHT]:
+                x += dist
+            if keys[K_UP]:
+                y -= dist
+            if keys[K_DOWN]:
+                y += dist
+            if keys[K_q]:
+                run = False
+    window.blit(ufo_img, (x, y))
+    display.update()
