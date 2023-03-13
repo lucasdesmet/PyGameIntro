4a92f58896002b45fe7ae00bd06e71df7945c2ef
Lucas De Smet <lucas.desmet@outlook.com>
2023-03-13 21:18:06 +0700

    Use raw strings to treat \ as literal character

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ display.set_caption("First application")
 
 # set window background
 background = transform.scale(image.load(
-    'pictures\\backgrounds\galaxy_2.jpg'), (w_width, w_height))
+    r'pictures\backgrounds\galaxy_2.jpg'), (w_width, w_height))
 window.blit(background, (0, 0))
 
 # add character
@@ -19,7 +19,7 @@ y = 100
 
 # draw.rect(window, (0, 0, 255), (x, y, width, height))
 img1 = transform.scale(image.load(
-    'pictures\heroes\\ufo_1.png'), (width, height))
+    r'pictures\heroes\ufo_1.png'), (width, height))
 window.blit(img1, (x, y))
 display.update()
 time.delay(5000)
